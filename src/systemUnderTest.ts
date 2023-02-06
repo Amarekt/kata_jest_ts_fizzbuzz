49e2a23fd8d7a4928c59c71de380aed3f1b8e468
Amarekt <zi.zou1@live.ca>
2023-02-06 01:07:36 -0500

    update

--- src/systemUnderTest.ts
+++ src/systemUnderTest.ts
@@ -1,7 +1,15 @@
 export class SystemUnderTest {
-    constructor() {
-    }
-    getMessage = (name: string) => {
-        return `Hello ${name}`
+    public num = 1;
+    fizzbuzz(){
+        for (let num; this.num <= 30; this.num++) {
+            if ((this.num % 5 == 0 && this.num % 3 == 0) || (this.num + '').includes('5') && (this.num + '').includes('3')) {
+                console.log("FizzBuzz");
+            } else if (this.num%3 == 0 || (this.num + '').includes('3')){
+                console.log("Fizz");
+            } else if (this.num%5 == 0 || (this.num + '').includes('5')){
+                console.log("Buzz");
+            } else  
+            console.log(this.num); 
+        }
     }
 }
--- test/systemUnderTest.test.ts
+++ test/systemUnderTest.test.ts
@@ -1,12 +1,34 @@
 import { SystemUnderTest } from "../src/systemUnderTest"
 
-test("given getMessage is called, when 'Jérôme' is passed, then 'Hello Jérôme' is returned", () => {
-    // arrange
-    const sut = new SystemUnderTest()
-
-    // act
-    const actual = sut.getMessage("Jérôme")
-
-    // assert
-    expect(actual).toEqual("Hello Jérôme")
+const sut = new SystemUnderTest()
+test("quand la valeur est de 3 ou il y a un 3 dans la valeur, Fizz est alors retourné", () => {
+    const result = [];
+    for (let i = 1; i <= 100; i++) {
+      if ((i % 5 === 0 && i % 3 === 0) || (i + '').includes('5') && (i + '').includes('3')) {
+        result.push("FizzBuzz");
+      } else if (i % 3 === 0 || (i + '').includes('3')) {
+        result.push("Fizz");
+      } else if (i % 5 === 0 || (i + '').includes('5')) {
+        result.push("Buzz");
+      } else {
+        result.push(i + '');
+      }
+    }
 })
+// test("quand la valeur est de 5 ou il y a un 5 dans la valeur, Fizz est alors retourné", () => {
+//     expect(sut.fizzbuzz()).toEqual("Buzz")
+// })
+// test("quand la valeur est de 5 ou il y a un 5 dans la valeur, Fizz est alors retourné", () => {
+//     expect(sut.fizzbuzz()).toEqual("Buzz")
+// })
+
+// test("given getMessage is called, when 'Jérôme' is passed, then 'Hello Jérôme' is returned", () => {
+//     // arrange
+//     const sut = new SystemUnderTest()
+
+//     // act
+//     const actual = sut.getMessage("Jérôme")
+
+//     // assert
+//     expect(actual).toEqual("Hello Jérôme")
+// })
